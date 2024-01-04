43ca5e327b7bfeddcba1c8c543bbf3c33a579268
Rohit Pal <palrohit2425@gmail.com>
2024-01-04 11:01:56 +0545

    JavaScript Variables and objects

---
+++ variables.js
@@ -0,0 +1,16 @@
+var a=20;
+const PI = 3.14;
+let x=25;
+let student ={
+                    fullName:"Rohit pal",
+                    age:21,
+                    class:15,
+                    isFollow:true
+               };
+const profile ={
+     
+          username: "@rohitpal",
+          followers: 223 ,
+          following: 123,
+          
+               };
