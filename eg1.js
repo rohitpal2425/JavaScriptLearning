3e140c54c87384e079e874bc4c3f2efdc61593d5
Rohit Pal <palrohit2425@gmail.com>
2024-01-07 10:49:57 +0545

    Example of prompt function

---
+++ eg1.js
@@ -0,0 +1,10 @@
+//Get user to  to input a number using prompt("Enter a number"). Check if it the number is multiple of 5 or not.
+let num= prompt("Enter a number:");
+if( num %5==0)
+{
+    console.log("The number is multiple of 5");
+}
+else
+{
+    consoole.log("The number is not multiple of 5");
+}
